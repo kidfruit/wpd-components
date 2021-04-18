1751893406af5095b3bfedb5f037be884249a268
fengkuaile <fengkuaile@cjwsjy.com.cn>
2021-04-18 16:25:24 +0800

    编辑行信息

--- packages/SimpleTable/src/SimpleTable.vue
+++ packages/SimpleTable/src/SimpleTable.vue
@@ -164,7 +164,7 @@ export default {
         return item;
       });
     },
-    afterChange(changes, source) {
+    afterChange(changes, source) {console.log(changes, source)
       if (changes == null) {
         return;
       }
