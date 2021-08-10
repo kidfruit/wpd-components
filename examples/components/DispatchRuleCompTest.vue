8b23d91ec45bf3b67b427d8dcb62868d1dcd6a93
huangliyao <huangliyao@cjwsjy.com.cn>
2021-08-10 17:18:35 +0800

    增加溪洛渡数据测试

--- examples/components/DispatchRuleCompTest.vue
+++ examples/components/DispatchRuleCompTest.vue
@@ -5,7 +5,7 @@
       :classes="classNames"
       :ruleData="ruleData"
       :editable="true"
-      :selectId="'60106980'"
+      :selectId="'60103165'"
       @getData="getData"
     />
   </div>
