a92eb684e707b5ba43c2718577b54c74e2803ae3
liuyang <823246840@qq.com>
2021-10-28 18:02:02 +0800

    MultiSeriesResult 多站点时序值展示控件

--- examples/components/MultiSeriesResultTest.vue
+++ examples/components/MultiSeriesResultTest.vue
@@ -1,11 +1,11 @@
 <template>
-  <div style="width: 100%">
+  <div style="width: 100%;height: 500px;">
     <multi-series-result
-        :classes="classNames"
-        :timeLag="timeLag"
-        :treeData="treeData"
-        :seriesData="seriesData"
-        @checkedNodeUpdate="checkedNodeUpdate"
+      :classes="classNames"
+      :timeLag="timeLag"
+      :treeData="treeData"
+      :seriesData="seriesData"
+      @checkedNodeUpdate="checkedNodeUpdate"
     />
   </div>
 </template>
@@ -23,20 +23,18 @@ export default {
       visible: true,
       classNames: ['test-multi-series'],
       treeData: mock_Tree,
-      timeLag:false,
-      seriesData:[fakeData],//当前默认勾选，tree上勾选变多后，这里也变多，
+      timeLag: false,
+      seriesData:[],//当前默认勾选，tree上勾选变多后，这里也变多，
     }
   },
   methods: {
-    checkedNodeUpdate(){
-      this.seriesData = [fakeData,fakeData,fakeData]
+    checkedNodeUpdate(checkedNode){
+      console.log(checkedNode)
+      this.seriesData = []
+      checkedNode.forEach(() => {
+        this.seriesData.push(fakeData)
+      })
     }
   },
 }
 </script>
-<style scoped lang="scss">
-.table {
-  width: 100%;
-  height: 300px;
-}
-</style>
--- packages/MultiSeriesResult/src/MultiSeriesResult.vue
+++ packages/MultiSeriesResult/src/MultiSeriesResult.vue
@@ -1,36 +1,109 @@
 <template>
-  <div
-    :class="classNames"
-  >
-    hello world
+  <div :class="[classes, 'multi-series-result']">
+    <div class="multi-series-result-left">
+      <a-tree
+        v-model="checkedNode"
+        :treeData="treeData"
+        :checkable="true"
+        :defaultExpandAll="true"
+      />
+    </div>
+    <div class="multi-series-result-right">
+      <div
+        class="series-result-wrapper"
+        v-for="(item, index) in seriesData"
+        :key="index"
+      >
+        <SeriesResult
+          :splitIndex="item.splitIndex"
+          :tableColumns="item.tableColumns"
+          :tableData="item.tableData"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-
+import SeriesResult from "../../SeriesResult/src/SeriesResult";
 export default {
   name: 'MultiSeriesResult',
-  components: {
-  },
+  components: { SeriesResult },
   props: {
     classes: {
       type: Array,
-      required: false,
+      required: false
+    },
+    timeLag: {
+      type: Boolean,
+      required: false
+    },
+    treeData: {
+      type: Array,
+      required: true
+    },
+    seriesData: {
+      type: Array,
+      required: true
     }
   },
   data() {
-    return {};
+    return {
+      checkedNode: []
+    }
   },
-  computed: {
-    classNames() {
-      return ['multi-series-result'].concat(this.classes)
+  watch: {
+    checkedNode: {
+      deep: true,
+      handler() {
+        this.$emit('checkedNodeUpdate', this.checkedNode)
+      }
     }
   },
   methods: {
 
   }
-};
+}
 </script>
 
-<style lang="scss" scoped>
+<style lang="less">
+.multi-series-result {
+  .multi-series-result-right {
+    .chart-switch-button {
+      display: none;
+    }
+    .chart-box {
+      .collapse-table {
+        display: none;
+      }
+    }
+    .table-box {
+      display: none;
+    }
+  }
+}
+</style>
+<style lang="less" scoped>
+.multi-series-result {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  .multi-series-result-left {
+    width: 200px;
+    height: 100%;
+    overflow: auto;
+    //background: rosybrown;
+  }
+  .multi-series-result-right {
+    width: calc(100% - 200px);
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    overflow: auto;
+    //background: yellowgreen;
+    .series-result-wrapper {
+      width: 50%;
+    }
+  }
+}
 </style>
