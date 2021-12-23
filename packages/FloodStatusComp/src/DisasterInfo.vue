46605f686e0f710865addbd5e4178104e27c23ac
liuyang <823246840@qq.com>
2021-12-23 19:33:26 +0800

    FLoodStatusComp 灾情

--- packages/FloodStatusComp/src/DisasterInfo.vue
+++ packages/FloodStatusComp/src/DisasterInfo.vue
@@ -6,15 +6,20 @@
         :treeData="treeData"
         :checkable="true"
         :defaultExpandAll="true"
+        @check="handleCheckTreeNode"
       />
     </div>
-    <div class="disaster-info-map"></div>
+    <div class="disaster-info-map">
+      <div class="map" id="map" :key="mapRandomKey"/>
+    </div>
   </div>
 </template>
 
 <script>
 import lodash from 'lodash'
+import * as echarts from 'echarts'
 import { v4 as uuidv4 } from 'uuid'
+import china from './china.json'
 export default {
   name: 'DisasterInfo',
   props: {
@@ -25,9 +30,12 @@ export default {
   },
   data() {
     this.copyDisasterInfo = lodash.cloneDeep(this.disasterInfo)
+    this.checkedProvince = []
+    this.regions = []
     return {
       treeData: [],
-      treeRandomKey: uuidv4()
+      treeRandomKey: uuidv4(),
+      mapRandomKey: uuidv4()
     }
   },
   mounted() {
@@ -36,6 +44,7 @@ export default {
   methods: {
     init() {
       this.initTree()
+      this.initMap()
     },
     initTree() {
       this.treeRandomKey = uuidv4()
@@ -45,7 +54,7 @@ export default {
         this.copyDisasterInfo.dangerList.forEach(val => {
           if (item === val.area) {
             children.push({
-              title: val.name,
+              title: `${val.name} ${val.maxWaterLevel} [${val.maxWaterTime}]`,
               key: val.id,
               ...val
             })
@@ -57,6 +66,51 @@ export default {
           children
         })
       })
+    },
+    initMap() {
+      echarts.registerMap('china', china)
+      let mapChartDom = document.getElementById('map')
+      let mapChart = echarts.init(mapChartDom)
+      let option = {
+        geo: {
+          regions: this.regions,
+          map: 'china',
+          aspectScale: 1,
+          zoom: 1.25,
+          itemStyle: {
+            emphasis: {
+              show: false,
+              areaColor: '#87cefa'
+            }
+          },
+        },
+      }
+      mapChart.setOption(option)
+    },
+    handleCheckTreeNode(checkedKeys, e) {
+      // console.log(checkedKeys, e)
+      this.checkedProvince = []
+      this.copyDisasterInfo.dangerArea.forEach(item => {
+        for (let i = 0; i < checkedKeys.length; i++) {
+          if (item === checkedKeys[i]) {
+            this.checkedProvince.push(item)
+          }
+        }
+      })
+      console.log(this.checkedProvince)
+      this.regions = []
+      this.checkedProvince.forEach(item => {
+        this.regions.push({
+          name: item,
+          itemStyle: {
+            areaColor: 'orange'
+          }
+        })
+      })
+      this.mapRandomKey = uuidv4()
+      this.$nextTick(() => {
+        this.initMap()
+      })
     }
   }
 }
@@ -66,12 +120,22 @@ export default {
 .disaster-info {
   width: 100%;
   height: 800px;
+  display: flex;
   .disaster-info-tree {
-    width: 200px;
+    width: 400px;
     height: 100%;
     text-align: left;
     padding: 25px;
     background: #f1f1f1;
+  }
+  .disaster-info-map {
+    width: calc(100% - 400px);
+    height: 100%;
+    padding: 25px;
+    .map {
+      width: 100%;
+      height: 100%;
+    }
   }
 }
 </style>
